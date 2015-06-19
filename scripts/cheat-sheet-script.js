78d11d2ea23fb57fc00f2277091fd3c60dad7686
Emilio Corpus <emiliocorpus@gmail.com>
2015-06-19 12:44:58 -0700

    ADD NEW FILES

---
+++ scripts/cheat-sheet-script.js
@@ -0,0 +1,2 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Teresa Nesteby & <a href=\"http://www.google.com\">Emilio Corpus</a>. All rights reserved.</p>";
