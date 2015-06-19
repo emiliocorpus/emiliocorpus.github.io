24b1c02d2250021d81f9b6e1d00c38fe8531d3c2
Emilio Corpus <emiliocorpus@gmail.com>
2015-06-19 15:51:14 -0700

    UPDATED FOR CHEAT SHEET

--- scripts/cheat-sheet-script.js
+++ scripts/cheat-sheet-script.js
@@ -1,2 +1,2 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Teresa Nesteby & <a href=\"http://www.google.com\">Emilio Corpus</a>. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " <a href=\"http://TeresaNesteby.github.io/index.html\">Teresa Nesteby</a> & <a href=\"http://emiliocorpus.github.io/index.html\">Emilio Corpus</a>. All rights reserved.</p>";
